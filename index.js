e15b64428d127a68190fcdd4125968a18968b890
Alex Verevkin <alex-verevkin@mail.ru>
2022-12-07 15:33:09 +0300

    add polindrome function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Hello')
+
+var isPalindrome = function(x) {
+    const stringX = String(x)
+    const reverse = [...stringX].reverse().join('')
+    console.log('reverse>>>', reverse)
+    if (stringX === reverse) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isPalindrome(121))
